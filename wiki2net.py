5bb9642a8f29894519f64aaf9f62b02440e72322
Telmo Menezes <telmo@telmomenezes.com>
2011-07-11 11:54:23 +0200

    working on xml parser

--- wiki2net.py
+++ wiki2net.py
@@ -26,6 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 import sys
 import sqlite3
 from xml.etree import ElementTree as ET
+import re
 
 
 def safe_execute(cur, query):
